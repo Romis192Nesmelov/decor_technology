ad61c5c004a4226f460d4945b9d430b6ba860f3a
Romis <romis@nesmelov.com>
2022-12-14 20:00:47 +0300

    done portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,13 +4,13 @@
     <section class="gray bg5 py-5">
         <div class="container">
             <div class="row align-items-stretch">
-                <div class="col-lg-4 col-md-6 col-xs-12">
+                <div class="col-lg-4 col-md-5 col-xs-12">
                     <div class="bg-white box">
                         <h2>{{ $job->name }}</h2>
                         {!! $job->description !!}
                     </div>
                 </div>
-                <div class="col-lg-8 col-md-6 col-sm-12 col-xs-12 row row-cols-auto mt-2">
+                <div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 row row-cols-auto">
                     @foreach($job->jobImages as $image)
                         <div class="col-lg-6 col-md-12 col-sm-12">
                             <div class="portfolio-image">
