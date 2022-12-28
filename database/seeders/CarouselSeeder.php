ef44893319c36891008e0e37d6872a037f08138d
Romis <romis@nesmelov.com>
2022-12-28 20:54:52 +0300

    fixes

--- database/seeders/CarouselSeeder.php
+++ database/seeders/CarouselSeeder.php
@@ -17,12 +17,12 @@ class CarouselSeeder extends Seeder
         $data = [
             [
                 'head' => 'Презентация VIVO V7/V7+',
-                'text' => 'Оформление презентации нового смартфона VIVO V7/V7+ в Конгресс-Парке.',
+                'text' => 'Оформление презентации нового смартфона VIVO V7/V7+ в Конгресс-Парке.',
                 'image' => 'image1.jpg'
             ],
             [
                 'head' => '«Алкон» в CRYSTAL BALLROOM',
-                'text' => 'Оформление конференции для компании «Алкон» в CRYSTAL BALLROOM.',
+                'text' => 'Оформление конференции для компании «Алкон» в CRYSTAL BALLROOM.',
                 'image' => 'image2.jpg'
             ],
             [
